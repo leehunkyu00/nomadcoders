cd0d8d55a64dc940290cffe936fb3634118596e1
이훈규 <leehunkyu00@gmail.com>
2019-01-24 23:18:58 +0900

    ending 2.3

--- wetube/index.js
+++ wetube/index.js
@@ -1,1 +1,10 @@
-console.log("hi");
+var express = require('express');
+var app = express();
+
+const PORT = 4000;
+
+const handleListening = () => {
+    console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening);
